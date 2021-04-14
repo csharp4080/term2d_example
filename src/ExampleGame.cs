d91a9c7360e51d1ab80625e4b459cedbdb5bca93
mshernandez5 <mshernandez505@gmail.com>
2021-04-13 23:22:33 -0700

    Rework for term2d 0.4.0.

--- src/ExampleGame.cs
+++ src/ExampleGame.cs
@@ -1,12 +1,12 @@
 using System;
 using term2d;
 
-namespace example
+namespace Term2DGame
 {
     class ExampleGame : Game
     {
-        int playerX = 20;
-        int playerY = 20;
+        int playerX = 1;
+        int playerY = 1;
 
         public static void Main(string[] args)
         {
@@ -18,38 +18,20 @@ namespace example
             canvas.DefaultBackgroundColor = ConsoleColor.Blue;
             canvas.DefaultForegroundColor = ConsoleColor.White;
             canvas.Clear();
+            playerX = canvas.Width / 2;
+            playerY = canvas.Height / 2;
         }
 
         double timer = 0.0;
+        bool running = true;
         override public bool Update(UpdateInfo updateInfo)
         {
             // Update Timer
             timer += updateInfo.DeltaTime;
-            // Handle Logic
-            if (updateInfo.HasUnreadInput)
-            {
-                switch (updateInfo.LastInput)
-                {
-                    case ConsoleKey.UpArrow:
-                        playerY--;
-                        break;
-                    case ConsoleKey.DownArrow:
-                        playerY++;
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        playerX--;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        playerX++;
-                        break;
-                    case ConsoleKey.Escape:
-                        return false;
-                    default:
-                        break;
-                }
-            }
+            
             // Update View
             Canvas canvas = updateInfo.ActiveCanvas;
+            
             // Alternate Between Blue & Green Every 2 Seconds
             if (timer > 5.0)
             {
@@ -63,46 +45,64 @@ namespace example
                 }
                 timer = 0.0;
             }
+
             canvas.Clear();
-            int width = canvas.GetWidth();
-            int height = canvas.GetHeight();
-            for (int row = 0; row < height; row++)
+            int width = canvas.Width;
+            int height = canvas.Height;
+            
+            // Draw Border Frame
+            canvas.Draw(0, 0, '╔');
+            canvas.Draw(0, width - 1, '╗');
+            canvas.Draw(height - 1, 0, '╚');
+            canvas.Draw(height - 1, width - 1, '╝');
+            for (int col = 1; col < width - 1; col++)
             {
-                for (int col = 0; col < width; col++)
-                {
-                    if (row == 0 && col == 0)
-                    {
-                        canvas.Draw(row, col, '╔');
-                    }
-                    else if (row == 0 && col == width - 1)
-                    {
-                        canvas.Draw(row, col, '╗');
-                    }
-                    else if (row == height - 1 && col == 0)
-                    {
-                        canvas.Draw(row, col, '╚');
-                    }
-                    else if (row == height - 1 && col == width - 1)
-                    {
-                        canvas.Draw(row, col, '╝');
-                    }
-                    else if (row == 0 || row == height - 1)
-                    {
-                        canvas.Draw(row, col, '═');
-                    }
-                    else if (col == 0 || col == width - 1)
-                    {
-                        canvas.Draw(row, col, '║');
-                    }
-                    else if (col == playerX && row == playerY)
-                    {
-                        canvas.Draw(row, col, '█', ConsoleColor.Red, ConsoleColor.Blue);
-                    }
-                }
+                canvas.Draw(0, col, '═');
+                canvas.Draw(height - 1, col, '═');
             }
+            for (int row = 1; row < height - 1; row++)
+            {
+                canvas.Draw(row, 0, '║');
+                canvas.Draw(row, width - 1, '║');
+            }
+
+            // Draw Player
+            if (playerX > 0 && playerX < canvas.Width - 1
+                && playerY > 0 && playerY < canvas.Height - 1)
+            {
+                canvas.Draw(playerY, playerX, '█', ConsoleColor.Red, ConsoleColor.Blue);
+            }
+
+            // Draw Status
             double actualFPS = 1.0 / updateInfo.DeltaTime;
             canvas.DrawText(0, 1, $"══ term2d demo ══ FPS: {actualFPS:0.0} ══ Timer: {timer:0.00} ══");
-            return true;
+            
+            // True To Continue Game Loop, False To Stop
+            return running;
+        }
+
+        override public void OnKeyEvent(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    playerY--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    playerY++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    playerX--;
+                    break;
+                case ConsoleKey.RightArrow:
+                    playerX++;
+                    break;
+                case ConsoleKey.Escape:
+                    running = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
